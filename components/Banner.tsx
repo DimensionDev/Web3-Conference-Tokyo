b6b0fedffe42b7eea9c2a0ca6453104c938a8940
billy <Billy@mask.io>
2022-01-12 01:42:31 +0800

    fix: fix

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,13 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import TitleImg from "../assets/title.png";
+import style from "../styles/Banner.module.css";
 const Banner: NextPage = () => {
-  return <div>Banner</div>;
+  return (
+    <div className={style.container}>
+      <Image className={style.title} src={TitleImg} alt="" />
+      <div>222</div>
+    </div>
+  );
 };
 export default Banner;
--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,5 +1,15 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import style from "../styles/Topbar.module.css";
+import LogoImg from '../assets/Logo.svg'
 const Topbar: NextPage = () => {
-  return <div>topbar</div>;
+  return <div className={style.container}>
+      <div className={style.logo}>
+          <Image src={LogoImg} alt="logo" />
+      </div>
+      <div className={style.rightBox}>
+          222
+      </div>
+  </div>;
 };
 export default Topbar;
