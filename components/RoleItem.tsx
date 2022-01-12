0c824553f250869c20d92458c463511bdea373fb
billy <Billy@mask.io>
2022-01-12 21:05:43 +0800

    fix: temp hover card

--- components/RoleItem.tsx
+++ components/RoleItem.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import style from "../styles/RoleItem.module.css";
 import Image from "next/image";
 import { Role } from "./dataProvider/role";
-import MoreImg from '../assets/more.svg'
+import MoreImg from "../assets/more.svg";
 interface Props {
   role: Role;
 }
@@ -10,18 +10,24 @@ const RoleItem: NextPage<Props> = ({ role }) => {
   const { avatar, name, roleTitles } = role;
   return (
     <div className={style.box}>
-      <div className={style.avatarBox}>
-        <Image width={'246px'} height={'246px'}  className={style.avatar} src={avatar} />
-      </div>
-      <div className={style.uInfo}>
-        {roleTitles.map((item, index) => {
-          return <div key={index}>{item}</div>;
-        })}
-      </div>
-      <div className={style.uName}>{name}</div>
-      <div className={style.more} >
-      <Image src={MoreImg} />
-
+      <div className={style.inner}>
+        <div className={style.avatarBox}>
+          <Image
+            width={"246px"}
+            height={"246px"}
+            className={style.avatar}
+            src={avatar}
+          />
+        </div>
+        <div className={style.uInfo}>
+          {roleTitles.map((item, index) => {
+            return <div key={index}>{item}</div>;
+          })}
+        </div>
+        <div className={style.uName}>{name}</div>
+        <div className={style.more}>
+          <Image src={MoreImg} />
+        </div>
       </div>
     </div>
   );
