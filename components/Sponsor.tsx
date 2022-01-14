bacbd1f3edb24ca78293d0612c76fa46e7b22d97
billy <Billy@mask.io>
2022-01-14 12:37:53 +0800

    feat: sponsor mobile

--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
 import style from "../styles/Sponsor.module.css";
+import SponsorImg from "../assets/sponsorBg.svg";
+import Image from "next/image";
 
-const Sponsor: NextPage= () => {
+const Sponsor: NextPage = () => {
   return (
-    <div className={style.container} id='メディアパートナー' >
-      <div className={style.inner}></div>
+    <div className={style.container} id="メディアパートナー">
+      <div className={style.inner}>
+        <Image src={SponsorImg} />
+      </div>
     </div>
   );
 };
