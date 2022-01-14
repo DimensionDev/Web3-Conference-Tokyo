3c9924a42e3896e95c385b8cde715f131ea246a9
billy <Billy@mask.io>
2022-01-14 18:01:09 +0800

    fix: temp ifx build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-  reactStrictMode: true,
-  assetPrefix: "",
-  // assetPrefix: '/Web3-Confrence-Tyoko'
-};
+  assetPrefix: !debug ? '/Web3-Conference-Tokyo/' : '',
+}
