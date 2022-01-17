33dada2246a09208d194dfaa5ec9c72e37804cff
billy <Billy@mask.io>
2022-01-17 14:50:56 +0800

    fix: style

--- components/TImeTable.tsx
+++ components/TImeTable.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from "next";
 import style from "../styles/TimeTable.module.css";
 import timeTablesMap from "./dataProvider/timetable";
-import MobileTableItem from "./MobileTableItem";
-import PreTimeInfo from "./PreTimeInfo";
+
 import TimeTableItem from "./TimeTableItem";
 const TimeTable: NextPage = () => {
   return (
@@ -12,24 +11,7 @@ const TimeTable: NextPage = () => {
         {timeTablesMap.map((item, index) => {
           return (
             <div key={`${index}_timeContainer`} style={{ width: "100%" }}>
-              {/* <TimeTableItem key={`${index}_timetable`} info={item} /> */}
-
-              {(globalThis || window).innerWidth > 768 ? (
-                   <TimeTableItem key={`${index}_timetable`} info={item} />
-              ) : (
-                // <MobileTableItem
-                //   key={`${index}_mobileTimetableItem`}
-                //   info={item}
-                // />
-                <div>22</div>
-              )}
-              {item.switchTime && (
-                <PreTimeInfo
-                  key={`${index}_pre`}
-                  text={item.switchTime}
-                  isBig={item.isBig}
-                />
-              )}
+              <TimeTableItem key={`${index}_timetable`} info={item} />
             </div>
           );
         })}
--- components/TimeTableItem.tsx
+++ components/TimeTableItem.tsx
@@ -4,6 +4,8 @@ import style from "../styles/TimeTableItem.module.css";
 import Image from "next/image";
 import LiveImgJa from "../assets/schedule/live_ja.svg";
 import RecordImgJa from "../assets/schedule/record_ja.svg";
+import MobileLiveJa from "../assets/mobile_live.svg";
+import MobileRecordJa from "../assets/mobile_record.svg";
 interface Props {
   info: TimeTableProp;
 }
@@ -11,56 +13,100 @@ const TimeTableItem: NextPage<Props> = ({ info }) => {
   const { preview, time, title, sub, extra } = info;
   return (
     <div className={style.container}>
-      <div className={style.inner}>
-        <div className={style.previewBox}>
-          <Image src={preview} className={style.previewImg} />
-          {info.difficuty && (
-            <div className={style.diffBox}>
-              <div>難易度</div>
-              <div className={style.whiteBtn}>{info.difficuty}</div>
-            </div>
-          )}
-          {info.isLive && (
-            <div className={style.previewTip}>
-              <Image src={LiveImgJa} />
-            </div>
-          )}
-          {info.isRecord && (
-            <div className={style.previewTip}>
-              <Image src={RecordImgJa} />
-            </div>
-          )}
-        </div>
-        <div className={style.infoBox}>
-          <div className={style.time}>{time}</div>
-          <div className={style.title}>{title}</div>
-          <div className={style.subTitle}>
-            {sub.map((item, index) => {
-              return (
-                <div key={index}>
-                  {item} <br />
-                  <br />
-                </div>
-              );
-            })}
+      {(globalThis || window).innerWidth > 769 ? (
+        <div className={style.inner}>
+          <div className={style.previewBox}>
+            <Image src={preview} className={style.previewImg} />
+            {info.difficuty && (
+              <div className={style.diffBox}>
+                <div>難易度</div>
+                <div className={style.whiteBtn}>{info.difficuty}</div>
+              </div>
+            )}
+            {info.isLive && (
+              <div className={style.previewTip}>
+                <Image src={LiveImgJa} />
+              </div>
+            )}
+            {info.isRecord && (
+              <div className={style.previewTip}>
+                <Image src={RecordImgJa} />
+              </div>
+            )}
           </div>
-          <div className={style.subBox}>
-            {extra.map((subBoxItem, index) => {
-              return (
-                <div key={index} className={style.subBoxItem}>
-                  <div className={style.subBoxType}>
-                    {subBoxItem.type || (
-                      <div className={style.hidden}>u cant see me</div>
-                    )}
+          <div className={style.infoBox}>
+            <div className={style.time}>{time}</div>
+            <div className={style.title}>{title}</div>
+            <div className={style.subTitle}>
+              {sub.map((item, index) => {
+                return (
+                  <div key={index}>
+                    {item} <br />
+                    <br />
                   </div>
-                  <div className={style.subBoxName}>{subBoxItem.name}</div>
-                  <div className={style.subBoxSub}>{subBoxItem.sub}</div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+            <div className={style.subBox}>
+              {extra.map((subBoxItem, index) => {
+                return (
+                  <div key={index} className={style.subBoxItem}>
+                    <div className={style.subBoxType}>
+                      {subBoxItem.type || (
+                        <div className={style.hidden}>u cant see me</div>
+                      )}
+                    </div>
+                    <div className={style.subBoxName}>{subBoxItem.name}</div>
+                    <div className={style.subBoxSub}>{subBoxItem.sub}</div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        222
+        // <div className={style.m_inner}>
+        //   <div className={style.mobileTime}>
+        //     <div className={style.mobileTimeUp}>
+        //       {info.isLive && <Image src={MobileLiveJa} />}
+        //       {info.isRecord && <Image src={MobileRecordJa} />}
+        //       <div className={style.m_time}>{time}</div>
+        //     </div>
+        //     {info.difficuty && (
+        //       <div className={style.m_whiteBtn}>{info.difficuty}</div>
+        //     )}
+        //   </div>
+        //   <div className={style.m_infoBox}>
+        //     <div className={style.m_title}>{title}</div>
+        //     <div className={style.m_subTitle}>
+        //       {sub.map((item, index) => {
+        //         return (
+        //           <div key={index}>
+        //             {item} <br />
+        //             <br />
+        //           </div>
+        //         );
+        //       })}
+        //     </div>
+        //     <div className={style.m_subBox}>
+        //       {extra.map((subBoxItem, index) => {
+        //         return (
+        //           <div key={index} className={style.m_subBoxItem}>
+        //             <div className={style.m_subBoxType}>
+        //               {subBoxItem.type || (
+        //                 <div className={style.m_hidden}>u cant see me</div>
+        //               )}
+        //             </div>
+        //             <div className={style.m_subBoxName}>{subBoxItem.name}</div>
+        //             <div className={style.m_subBoxSub}>{subBoxItem.sub}</div>
+        //           </div>
+        //         );
+        //       })}
+        //     </div>
+        //   </div>
+        // </div>
+      )}
     </div>
   );
 };
