9ed3e897c3f837c089c9b7e5e1b79cda7a329cd7
billy <Billy@mask.io>
2022-01-18 01:18:05 +0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/5--O6bt3WxDm7nvnyKj2W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-3a9c1c5061f5ea1f.js","static/css/329d14f62aad8560.css","static/chunks/pages/index-7f4f91f40e06250b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- out/_next/static/ZX5p_EwDVJLPG8W_NCF_G/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-3a9c1c5061f5ea1f.js","static/css/e838a69f58b7473e.css","static/chunks/pages/index-b501e352438a5ca3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
