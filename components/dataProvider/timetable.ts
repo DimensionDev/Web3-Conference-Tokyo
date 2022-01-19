51f1f001f8e2f52b42f2ca9dfbdb842538860bb6
billy <Billy@mask.io>
2022-01-19 16:16:59 +0800

    fix: timeTable locales temp

--- components/dataProvider/timetable.ts
+++ components/dataProvider/timetable.ts
@@ -36,41 +36,34 @@ const func: () => TimeTableProp[] = () => {
       time: "11:00-11:10",
       title: t("timeTable_1_t"),
       sub: [],
-      extra: [{ type: "登壇者", name: "Nachan", sub: "MaskNetwork" }],
+      extra: [{ type: t("speaker"), name: "Nachan", sub: "MaskNetwork" }],
       isLive: true,
       // switchTime: "5分で転換",
     },
     {
       preview: img2,
       time: "11:15-11:30",
-      title: "インターネットの歴史とMaskNetworkの紹介",
-      sub: [
-        "・Web1-Web2-Web3 の歴史を振り返りながらMaskの紹介",
-        "・未来のWebの予想",
-      ],
-      extra: [{ type: "登壇者", name: "SujiYan", sub: "MaskNetwork CEO" }],
+      title: t("timeTable_2_t"),
+      sub: [t("timeTable_2_s"), t("timeTable_2_s1")],
+      extra: [{ type: t("speaker"), name: "SujiYan", sub: "MaskNetwork CEO" }],
       isRecord: true,
       // switchTime: "11:30-11:35転換",
     },
     {
       preview: img3,
       time: "11:35-12:05",
-      title: "オープンソースソフトウェアの歴史からみるWeb3について",
-      sub: [
-        "・Web1の時のOSSとWeb3のOSSの変化",
-        "・新しいWebの時代で何ができるのか",
+      title: t("timeTable_3_t"),
+      sub: [t("timeTable_3_s"), t("timeTable_3_s1")],
+      extra: [
+        { type: t("speaker"), name: t("timeTable_3_r"), sub: "DeFiGeek" },
       ],
-      extra: [{ type: "登壇者", name: "猫井夕菜", sub: "DeFiGeek" }],
       isLive: true,
     },
     {
       preview: img4,
       time: "12:05-12:35",
-      title: "Web3を語るなら、まずDeFiを学ぼう",
-      sub: [
-        "・Web3で重要なポイントとなるDeFi(分散型金融)とは？",
-        "・DeFiの思想が新しい経済を作り出す",
-      ],
+      title: t("timeTable_4_t"),
+      sub: [t("timeTable_4_s"), t("timeTable_4_s1")],
       extra: [
         { type: "Mod", name: "Mai Fujimoto", sub: "株式会社グラコネ CEO" },
         {
@@ -85,12 +78,8 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img5,
       time: "12:40-13:20",
-      title: "セッション１:Web3.0が、Web1.0の理念を復活できるのか？",
-      sub: [
-        "・Web1時代に自由で検閲されない文化をWeb3で復帰することができるのか？",
-        "・Web3.0が完成系に近づいたとき、Web2.0はどうなってるのか",
-        "・インターネットの最終形について考える",
-      ],
+      title: t("timeTable_5_t"),
+      sub: [t("timeTable_5_s"), t("timeTable_5_s1"), t("timeTable_5_s2")],
       extra: [
         { type: "Mod", name: "馬渕さん", sub: "PwC" },
         { type: "Speaker", name: "安宅さん", sub: "Tokyo Otaku Mode" },
@@ -103,12 +92,12 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img6,
       time: "13:25-13:55",
-      title: "セッション２:既存IPの効果的なweb3.0へのアプローチ方法",
+      title: t("timeTable_6_t"),
       sub: [
-        "・既存IPがWeb3とのコラボで成功した事例",
-        "・日本でできるNFTの可能性",
-        "・アニメ、ゲーム、芸能etc これから参入方法は？",
-        "・持続可能にするためにどんな工夫が必要か",
+        t("timeTable_6_s"),
+        t("timeTable_6_s1"),
+        t("timeTable_6_s2"),
+        t("timeTable_6_s3"),
       ],
       extra: [
         {
@@ -128,12 +117,8 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img7,
       time: "14:30-15:00",
-      title: "セッション３:Web3.0事業例①インフラ",
-      sub: [
-        "・それぞれの事業内容/投資先で期待しているところ",
-        "・今後どのような企業に参入してほしいか",
-        "・これから必要になるWeb3インフラサービスとは",
-      ],
+      title: t("timeTable_7_t"),
+      sub: [t("timeTable_7_s"), t("timeTable_7_s1"), t("timeTable_7_s2")],
       extra: [
         { type: "Mod", name: "加藤 順弥", sub: "TOKEN ECONOMIST 代表" },
         { type: "Speaker", name: "国光宏尚", sub: "gumi Founder" },
@@ -147,12 +132,12 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img8,
       time: "15:05-15:35",
-      title: "セッション４:Web3.0事業例②メタバース/NFT関連",
+      title: t("timeTable_8_t"),
       sub: [
-        "・分散型のメタバースと既存のVRコミュニティがどのように交わっていきたいか",
-        "・メタバース/XR領域でのNFTの活用の仕方",
-        "・今後のメタバース業界展開予想",
-        "・エンターテイメントの可能性",
+        t("timeTable_8_s"),
+        t("timeTable_8_s1"),
+        t("timeTable_8_s2"),
+        t("timeTable_8_s3"),
       ],
       extra: [
         {
@@ -171,7 +156,7 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img9,
       time: "16:05-16:15",
-      title: "社会実装に使えるブロックチェーン技術(国内)",
+      title: t("timeTable_9_t"),
       sub: [],
       extra: [{ type: "Mod", name: "Chris Dai", sub: "Unicask" }],
       isLive: true,
@@ -180,17 +165,17 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img10,
       time: "16:20-16:50",
-      title: "社会実装に使えるブロックチェーン技術(海外)",
+      title: t("timeTable_10_t"),
       sub: [],
-      extra: [{ type: "登壇者", name: "Numbers protocol", sub: "" }],
+      extra: [{ type: t("speaker"), name: "Numbers protocol", sub: "" }],
       isRecord: true,
       // switchTime: "16:50-16:55  転換",
     },
     {
       preview: img11,
       time: "16:55-17:55",
-      title: "セッション５:超スタートアップピッチタイム",
-      sub: ["・Web3関連スタートアップ企業の紹介"],
+      title: t("timeTable_11_t"),
+      sub: [t("timeTable_11_s")],
       extra: [
         { type: "Mod", name: "亀井 聡彦", sub: "Fracton Ventures Co-Founder" },
       ],
@@ -200,12 +185,8 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img12,
       time: "16:55-17:55",
-      title: "セッション６:DAOはどう世界を変えていくのか",
-      sub: [
-        "・DAOは私たちの生活をどう変えてくれるの？",
-        "・DAOで稼ぐためにはどんなスキルが必要か",
-        "・DAOの最終系とは",
-      ],
+      title: t("timeTable_12_t"),
+      sub: [t("timeTable_12_s"), t("timeTable_12_s1"), t("timeTable_12_s2")],
       extra: [
         { type: "Mod", name: "新井 進悟", sub: "CRYPTO TIMES 編集長" },
         { type: "Speaker", name: "Makoto Inoue", sub: "ENS Developer" },
@@ -221,10 +202,10 @@ const func: () => TimeTableProp[] = () => {
     {
       preview: img13,
       time: "18:35-18:45",
-      title: "エンディング",
+      title: t("timeTable_13_t"),
       sub: [],
       extra: [
-        { type: "主催", name: "Nachan", sub: "" },
+        { type: "Organizer", name: "Nachan", sub: "" },
         { type: "", name: "Nefer", sub: "" },
         { type: "", name: "Vivian", sub: "" },
         { type: "", name: "MissBitcoin  Mai", sub: "" },
