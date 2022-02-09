c1eab275f770e15997525c840ffc69bd9dabac3e
billy <Billy@mask.io>
2022-02-09 14:27:50 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/yJbT-cpRqgoupBwdzXe88/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-3a9c1c5061f5ea1f.js","static/css/9805ca017238dacd.css","static/chunks/pages/index-edfba8bc0231a8f3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
