56fe481b7c78857593329363cc4c3337fea1cb40
billy <Billy@mask.io>
2022-02-09 16:00:01 +0800

    fix: typo

--- components/VideoAchive.tsx
+++ components/VideoAchive.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import BadgeImage from "../assets/video/badge.png";
-import LeftImage from '../assets/video/lefT.png'
+import LeftImage from "../assets/video/lefT.png";
 import style from "../styles/VideoAchive.module.css";
 import i18next from "i18next";
 
@@ -9,24 +9,21 @@ const maskYoutubeChannelUrl =
   "https://www.youtube.com/channel/UCqT1gH1Wy8Nb6ABYpi09hWA";
 const web3ConferenceYoutubeUrl = "https://www.youtube.com/embed/ZFd_Gd9NURY";
 const VideoAchive: NextPage = () => {
-  const {t} = i18next
+  const { t } = i18next;
   return (
-    <div id='youtube' className={style.container}>
+    <div id="youtube" className={style.container}>
       <div className={style.inner}>
         <div className={style.left}>
-          <div className={style.left_title} > 
-          <Image src={LeftImage} />
-
+          <div className={style.left_title}>
+            <Image src={LeftImage} />
           </div>
-          <div className={style.left_sub}>
-            {t('video_2_t')}
-          </div>
+          <div className={style.left_sub}>{t("video_2_t")}</div>
           <a
             href={maskYoutubeChannelUrl}
             target="_blank"
             className={style.left_btn}
           >
-             {t('video_t')}
+            {t("video_t")}
           </a>
         </div>
         <div className={style.right}>
