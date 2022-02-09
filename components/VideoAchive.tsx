c94cd5315bc3a14892a15b3b350f378a36ec62fe
billy <Billy@mask.io>
2022-02-09 15:58:36 +0800

    fix: lang

--- components/VideoAchive.tsx
+++ components/VideoAchive.tsx
@@ -3,29 +3,30 @@ import Image from "next/image";
 import BadgeImage from "../assets/video/badge.png";
 import LeftImage from '../assets/video/lefT.png'
 import style from "../styles/VideoAchive.module.css";
+import i18next from "i18next";
 
 const maskYoutubeChannelUrl =
   "https://www.youtube.com/channel/UCqT1gH1Wy8Nb6ABYpi09hWA";
 const web3ConferenceYoutubeUrl = "https://www.youtube.com/embed/ZFd_Gd9NURY";
 const VideoAchive: NextPage = () => {
+  const {t} = i18next
   return (
     <div id='youtube' className={style.container}>
       <div className={style.inner}>
         <div className={style.left}>
-          {/* <div className={style.left_title}>Check it Out!</div> */}
           <div className={style.left_title} > 
           <Image src={LeftImage} />
 
           </div>
           <div className={style.left_sub}>
-            YouTubeにて録画映像を公開しております いつでもお楽しみ頂けます。
+            {t('video_2_t')}
           </div>
           <a
             href={maskYoutubeChannelUrl}
             target="_blank"
             className={style.left_btn}
           >
-            Mask Network JPチャンネル
+             {t('video_t')}
           </a>
         </div>
         <div className={style.right}>
